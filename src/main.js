4853330b890e3d775bf68076ece2bc4f57a11e59
Franco Rojas <64764807+frandev1@users.noreply.github.com>
2025-01-18 00:59:21 -0600

    add Tailwind CSS and PostCSS configuration; update index.html title and replace main.js content with login form

--- src/main.js
+++ src/main.js
@@ -1,24 +1,21 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+  <div class="flex justify-center items-center h-screen bg-indigo-900">
+    <div class="w-96 p-6 shadow-lg bg-white rounded-md">
+      <h1 class="text-2xl font-bold text-center">Log In</h1>
+      <hr class="mt-3"/>
+      <div class="mt-3">
+        <label form="username" class="block text-base mb-2">Correo</label>
+        <input type="text" id="username" class="border w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600" placeholder="Ingresa el correo electrónico"/>
+      </div>
+      <div class="mt-3">
+        <label form="password" class="block text-base mb-2">Contraseña</label>
+        <input type="password" id="password" class="border w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600" placeholder="Ingresa la contraseña"/>
+      </div>
+      <div class="mt-3">
+        <button class="w-full bg-indigo-600 text-white text-base font-semibold py-2 rounded-md hover:bg-indigo-700">Log In</button>
+      </div>
+    </div> 
   </div>
 `
-
-setupCounter(document.querySelector('#counter'))
